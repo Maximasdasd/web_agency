4458870bf9a49b87d6fe7b5bbbbe4f5d466cfbf5
Maximasdasd <pokoratel228@gmail.com>
2026-06-03 18:00:27 +0300

    18.00 03.06

--- api/app/api/ticket.py
+++ api/app/api/ticket.py
@@ -10,3 +10,9 @@ router = APIRouter()
 @router.get("/get_all_tickets", response_model=Page[TicketResponse])
 def get_all_tickets(controller: TicketController = Depends(get_controllers)):
     return controller.get_all_tickets()
+
+
+
+@router.post("/create_ticket", response_model=TicketResponse, status_code=201)
+def create_ticket(ticket_data:TicketCreate, controller: TicketController = Depends(get_controllers)):
+    return controller.create_ticket(ticket_data)
--- api/app/controllers/ticket.py
+++ api/app/controllers/ticket.py
@@ -14,3 +14,16 @@ class TicketController:
     def get_all_tickets(self):
         query = select(TicketModel).order_by(TicketModel.id_ticket)
         return paginate(self.db, query)
+    
+    def create_ticket(self, ticket_data: TicketCreate):
+            
+        new_ticket = TicketModel(
+            name=ticket_data.name,
+            email=ticket_data.email,
+            phone=ticket_data.phone,
+            telegram=ticket_data.telegram,
+            category=ticket_data.category
+        )
+        self.db.add(new_ticket)
+        self.db.commit()
+        return new_ticket
